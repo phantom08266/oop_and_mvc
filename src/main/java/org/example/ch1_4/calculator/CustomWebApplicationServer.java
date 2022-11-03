84d02cd9d7cc5db2db9812772907ae2de18afea6
이준희 <qzqz08266@bodycodi.com>
2022-11-03 10:30:36 +0900

    웹 서버를 직접 만들어서 HTTP 요청 메시지 포멧을 확인

--- src/main/java/org/example/ch1_4/calculator/CustomWebApplicationServer.java
+++ src/main/java/org/example/ch1_4/calculator/CustomWebApplicationServer.java
@@ -3,6 +3,11 @@ package org.example.ch1_4.calculator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+
 
 public class CustomWebApplicationServer {
 
@@ -11,5 +16,31 @@ public class CustomWebApplicationServer {
 
     public CustomWebApplicationServer(int port) {
         this.port = port;
+    }
+
+    public void start() throws IOException {
+        try (ServerSocket serverSocket = new ServerSocket(port)) {
+            logger.info("[CustomWebApplicationServer] started {} port.", port);
+            Socket clientSocket;
+            logger.info("[CustomWebApplicationServer] waiting for client.");
+
+            while ((clientSocket = serverSocket.accept()) != null) {
+                logger.info("[CustomWebApplicationServer] client connected");
+
+                /**
+                 * Step1 - 사용자 요청을 메인 Thread가 처리하도록 한다.
+                 */
+
+                try (InputStream in = clientSocket.getInputStream(); OutputStream out = clientSocket.getOutputStream()) {
+                    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(in, StandardCharsets.UTF_8));
+                    DataOutputStream dataOutputStream = new DataOutputStream(out);
+
+                    String line;
+                    while((line = bufferedReader.readLine())!= "") {
+                        System.out.println(line);
+                    }
+                }
+            }
+        }
     }
 }
